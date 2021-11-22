2d6d8de4a8f5ac0bbc1ce9cda48b78589d1f7d56
Class Account <ee106a-age@c105-8.local>
2021-11-22 14:54:44 -0800

    Main and vision logic

--- src/gobears/src/gobears.py
+++ src/gobears/src/gobears.py
@@ -1,5 +1,27 @@
 import rospy
 import sys
 
+def gobears():
+	#Initialize dig client
+
+	#While not dig done: call dig callback on time interval until done.
+	#If failure, terminate, but if success, continue
+
+	#Initialize plant client
+
+	#While not plant done: call plant callback on time interval until done.
+	#If failure, terminate, but if success, continue
+
+
+	#Initialize fill client
+
+	#While not fill done: call fill callback on time interval until done.
+	#If failure, terminate, but if success, continue
+
+	#Initialize water client
+
+	#While not water done: call plant callback on time interval until done.
+	
 def main():
 	rospy.init_node('gobears')
+	gobears()
--- src/gobears/src/vision.py
+++ src/gobears/src/vision.py
@@ -1,5 +1,12 @@
 import rospy
 import sys
 
+#Request has name of object and camera
+def vision_callback(request):
+	#Switch statements for object == something, then use certain detector logic
+
+def vision_server():
+
 def main():
 	rospy.init_node('vision')
+	vision_server()
