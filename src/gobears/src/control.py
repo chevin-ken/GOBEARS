bd4fe104540218e7831963bc45061fd167fad295
Class Account <ee106a-age@c105-8.local>
2021-11-22 14:39:07 -0800

    Dig commented code structure

--- src/gobears/src/control.py
+++ src/gobears/src/control.py
@@ -1,13 +1,131 @@
 import rospy
 import actionlib
 import sys
+import moveit_commander
 
-from moveit_msgs.msg import MoveGroupAction, MoveGroupGoal, MoveGroupFeedback, MoveGroupResult, JointConstraint, Constraints
+from moveit_msgs.msg import OrientationConstraint, Constraints, CollisionObject, MoveGroupAction, MoveGroupGoal, MoveGroupFeedback, MoveGroupResult, JointConstraint
+from geometry_msgs.msg import PoseStamped
+from shape_msgs.msg import SolidPrimitive
+
+class PathPlanner(object):
+    """
+    Path Planning Functionality for Baxter/Sawyer
+
+    We make this a class rather than a script because it bundles up 
+    all the code relating to planning in a nice way thus, we can
+    easily use the code in different places. This is a staple of
+    good object-oriented programming
+
+    Fields:
+    _robot: moveit_commander.RobotCommander; for interfacing with the robot
+    _scene: moveit_commander.PlanningSceneInterface; the planning scene stores a representation of the environment
+    _group: moveit_commander.MoveGroupCommander; the move group is moveit's primary planning class
+    _planning_scene_publisher: ros publisher; publishes to the planning scene
+
+
+    """
+    def __init__(self, group_name):
+        """
+        Constructor.
+
+        Inputs:
+        group_name: the name of the move_group.
+            For Baxter, this would be 'left_arm' or 'right_arm'
+            For Sawyer, this would be 'right_arm'
+        """
+
+        # If the node is shutdown, call this function    
+        rospy.on_shutdown(self.shutdown)
+
+        # Initialize moveit_commander
+        moveit_commander.roscpp_initialize(sys.argv)
+
+        # Initialize the robot
+        self._robot = moveit_commander.RobotCommander()
+
+        # Initialize the planning scene
+        self._scene = moveit_commander.PlanningSceneInterface()
+
+        # This publishes updates to the planning scene
+        self._planning_scene_publisher = rospy.Publisher('/collision_object', CollisionObject, queue_size=10)
+
+        # Instantiate a move group
+        self._group = moveit_commander.MoveGroupCommander(group_name)
+
+        # Set the maximum time MoveIt will try to plan before giving up
+        self._group.set_planning_time(5)
+
+        # Set the bounds of the workspace
+        self._group.set_workspace([-2, -2, -2, 2, 2, 2])
+
+        # Sleep for a bit to ensure that all inititialization has finished
+        rospy.sleep(0.5)        self._group = moveit_commander.MoveGroupCommander(group_name)
+
+
+    def shutdown(self):
+        """
+        Code to run on shutdown. This is good practice for safety
+
+        Currently deletes the object's MoveGroup, so that further commands will do nothing
+        """
+        self._group = None
+        rospy.loginfo("Stopping Path Planner")
+
+    def plan_to_pose(self, target, orientation_constraints):
+        """
+        Generates a plan given an end effector pose subject to orientation constraints
+
+        Inputs:
+        target: A geometry_msgs/PoseStamped message containing the end effector pose goal
+        orientation_constraints: A list of moveit_msgs/OrientationConstraint messages
+
+        Outputs:
+        path: A moveit_msgs/RobotTrajectory path
+        """
+
+        self._group.set_pose_target(target)
+        self._group.set_start_state_to_current_state()
+
+        constraints = Constraints()
+        constraints.orientation_constraints = orientation_constraints
+        self._group.set_path_constraints(constraints)
+
+        plan = self._group.plan()
+
+        return plan
+
+    def execute_plan(self, plan):
+        """
+        Uses the robot's built-in controllers to execute a plan
+
+        Inputs:
+        plan: a moveit_msgs/RobotTrajectory plan
+        """
+
+        return self._group.execute(plan, True)
+
+class ControllerServer(object):
+	def __init__(planner):
+		#Initialize planner
+
+#Request: takes in pose and orientation constraints
+#Returned message: success/failure
+def callback(request):
+	#Initialize planner
+
+	#Execute trajectory with planner
+
+	#Return success/failure
+
+def server():
+	#Initialize node and server
+
+	#Spin
+
 
 def main():
 	rospy.init_node('control')
 
-	#Create subscriber to subscribe to node that receives position
-
-	#Create publisher to publish success/failure of action and final configuration
+	server()
+	
 
--- src/gobears/src/dig.py
+++ src/gobears/src/dig.py
@@ -1,5 +1,31 @@
 import rospy
 import sys
 
+done = False
+success = False
+
+#Returned message is combination of done and success
+def callback():
+	return
+
+def gobears_dig():
+	#Initialize vision client
+
+	#Initialize control client
+
+	#Initialize dig server
+
+	#1: Pick up trowel
+	#Call vision to find trowel, compute final orientation, call control to move to desired pose, close gripper
+
+	#2: Scoop Loop
+		#2.1: Find position of bean chunks, calculate desired pose, move to pose
+		#2.2 Execute predefined joint motion for scooping
+		#2.3 Find location of bin to put in loose bean chunks, calculate desired pose, move to pose
+		#2.4 Execute predefined joint motion for dropping into bin
+		#2.5 Call vision node to check size of hole, if vision node determines done, then terminate
+
+
 def main():
 	rospy.init_node('dig')
+	gobears_dig():
