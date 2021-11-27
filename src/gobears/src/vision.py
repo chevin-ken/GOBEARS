e1fa1f933e8fab65993b7353d263892e5c18793c
Kevin Chen <chevin@peters-imac.lan>
2021-11-27 12:03:50 -0800

    Vision node template code

--- src/gobears/src/vision.py
+++ src/gobears/src/vision.py
@@ -1,11 +1,31 @@
 import rospy
 import sys
 
+def get_head_orientation():
+	pose = None
+	return pose
+
+def get_hand_orientation():
+	pose = None
+	return pose
+
+#Use color for object detection
+def detect_object(detector, color):
+	head_point, hand_point = detector.detect(color)
+	head_pose = get_head_orientation()
+	hand_pose = get_hand_orientation()
+	return calculate_world_frame_coordinates(head_point, head_pose, hand_point, hand_pose)
+
+def calculate_world_frame_coordinates(head_point, head_pose, hand_point, hand_pose):
+	return
+
 #Request has name of object and camera
 def vision_callback(request):
 	#Switch statements for object == something, then use certain detector logic
+	return
 
 def vision_server():
+	return
 
 def main():
 	rospy.init_node('vision')
